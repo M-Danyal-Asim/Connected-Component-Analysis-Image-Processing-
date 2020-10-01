3122905a0cf62ee54c5bfb7b20d617f5d04df28a
Zain Ul Abaidin <42282178+Zainulabaidin5566@users.noreply.github.com>
2020-10-02 00:19:45 +0500

    Update connectedcomponenetanalysis.py
    removed useless lines

--- connectedcomponenetanalysis.py
+++ connectedcomponenetanalysis.py
@@ -6,20 +6,15 @@ dimensions = np.shape(image_1)
 width = image_1.shape[0]
 height = image_1.shape[1];
 image_2 = np.zeros(dimensions, dtype=np.uint16)
-
-
 # thresholding our image to get it into the binary
-
 for row in range(0, width):
     for col in range(0, height):
         if image_1[row][col] >= 127:
             image_1[row][col] = 1
         else:
             image_1[row][col] = 0
-
 print(image_1)
 cv.imwrite('Picture.png', image_1)
-
 equivalency_list = [0]
 Label = 0
 for row in range(0, dimensions[0]):
@@ -45,9 +40,4 @@ for row in range(0, dimensions[0]):
                 for p in range(np.shape(equivalency_list)[0]):
                     if equivalency_list[p] == b:
                         equivalency_list[p] = a
-
-
-        
-
-
 print("No of Objects in image are:", np.count_nonzero(np.unique(equivalency_list)))
